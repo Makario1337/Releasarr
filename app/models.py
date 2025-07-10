5594acb56642c5943acdc917594b5df7d38b40ff
Jonas <jonas@eschbach.pro>
2025-07-10 15:15:11 +0200

    1.2 Update UI Update Settings Minor fixes

--- app/models.py
+++ app/models.py
@@ -2,9 +2,10 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from .db import Base
 
+
 class Artist(Base):
     __tablename__ = "artist"
-    
+
     Id = Column(Integer, primary_key=True, index=True)
     Name = Column(String, index=True, nullable=False)
     StartYear = Column(Integer)
@@ -13,35 +14,42 @@ class Artist(Base):
     MusicbrainzId = Column(String, unique=True, index=True)
     DeezerId = Column(String, unique=True, index=True)
     DiscogsId = Column(String, unique=True, index=True)
-    SpotifyId = Column(String, unique=True, index=True)  # changed from Integer
+    SpotifyId = Column(String, unique=True, index=True)
+    AppleMusicId = Column(String, unique=True, index=True)
+    TidalId = Column(String, unique=True, index=True)
+    ImageUrl = Column(String, nullable=True)
     AlbumCount = Column(Integer)
     TrackFileCount = Column(Integer)
     TotalTrackFileCount = Column(Integer)
-    
-    releases = relationship("Release", back_populates="artist", cascade="all, delete-orphan")
+
+    releases = relationship(
+        "Release", back_populates="artist", cascade="all, delete-orphan"
+    )
 
 
 class Release(Base):
     __tablename__ = "release"
-    
+
     Id = Column(Integer, primary_key=True, unique=True, index=True)
     Title = Column(String, nullable=False)
     Year = Column(Integer)
     ArtistId = Column(Integer, ForeignKey("artist.Id"), nullable=False)
     cover_url = Column(String, nullable=True)
-    
+
     artist = relationship("Artist", back_populates="releases")
-    tracks = relationship("Track", back_populates="release", cascade="all, delete-orphan")
+    tracks = relationship(
+        "Track", back_populates="release", cascade="all, delete-orphan"
+    )
 
 
 class Track(Base):
     __tablename__ = "track"
-    
+
     Id = Column(Integer, primary_key=True)
     Title = Column(String, nullable=False)
     Length = Column(Integer)
     Featured_Artists = Column(String)
     SizeOnDisk = Column(Integer)
-    
+
     ReleaseId = Column(Integer, ForeignKey("release.Id"), nullable=False)
     release = relationship("Release", back_populates="tracks")
--- app/routers/index.py
+++ app/routers/index.py
@@ -4,6 +4,7 @@ from fastapi.templating import Jinja2Templates
 router = APIRouter()
 templates = Jinja2Templates(directory="app/templates")
 
+
 @router.get("/")
 def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
